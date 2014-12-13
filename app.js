3b649aa6a3d7e306e8aabf1dd02acaf63326a3ae
Borgaard <nicoleborgaard@gmail.com>
2014-12-12 23:23:50 -0800

    Add db.js, add views, posts, sessions and users. Establish db in postgres

--- app.js
+++ app.js
@@ -1,164 +1,145 @@
-/* Blog requirements
+var express = require('express'),
+		ejs 		= require('ejs'),
+		app			= express(),
+		path		= require('path'),
+		bodyParser 	  = require('body-parser'),
+		cookieParser  = require('cookie-parser'),
+		session       = require('express-session'),
+		LocalStrategy = require('passport-local').Strategy,
+		passport      = require('passport'),
+		db						= require('./db.js'),
+		methodOverride = require('method-override');
 
-2 tables, 2 data sets we're dealing with
-users table - parent table
-posts table - child table
-
-first build users table + authentication
-
-
-
-POST always has to do with creation of data
-GET always has to do with editing data
-PATCH updates db
-edit just renders the form to allow you to update, happens in frontend
-update updates the db. no one "edits" the db.
-
-bad project uses GET for creating data. avoid!!!!
-
-== STEPS TO MAKE BLOG WITH RESTFUL ROUTING ==
-GET /users/:id 
-GET /users/add
-POST /users
-GET /posts/:id
-GET /posts/:id/edit
-POST /posts - create a new post
-PATCH /posts/:id
-DELETE /posts/:id
-GET /posts/new
-GET /posts - views the post
-
-DATABASE reviewblog
-TABLE users
-id         SERIAL PRIMARY KEY INT
-username   VARCHAR()
-email      VARCHAR()
-password   VARCHAR()
-TABLE posts
-id         INT
-title      VARCHAR()
-mainbody   TEXT
-refuser_id INT
-
-
-
-
-
-
-
-*/
-
-
-
-
-
-
-//express is a layer on top of node that allows us to use ...err...serveerr? uh. research this.
-var express = require('express');
-//function runs to instantiate express
-var app     = express();
-//ejs is templating engine to render our pages
-var ejs           = require('ejs');
-var path		      = require('path');
-var bodyParser 	  = require('body-parser');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-var LocalStrategy = require('passport-local').Strategy;
-var passport      = require('passport');
-var db            = require('./db.js');
-
-//express knows we want to use ejs
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':true}));
 app.use(session({
-	secret: 'keyboard cat',
-	resave: false,
-	saveUninitialized: true
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(3000, function() {
-	console.log("Listening on 3000!")
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-
-app.get('/', function(req, res) {
-	res.render('index'); //shows index landing page on root
-});
-
-//user goes to url /users/new and OUR FILE /new.ejs is rendered
-app.get('/users/new', function(req, res) {
-	res.render('users/new'); //file path we're rendering matches url, not something we can rely on. route has nothing to do with file or view we're rendering.
-});
-
-app.post('/users', function(req, res) {
-	db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [req.body.username, req.body.email, req.body.password], function(err, dbRes) { //callback with error and db response/result
+passport.deserializeUser(function(id, done) {
+	db.query('SELECT * FROM users WHERE id = $1', [id], function(err, dbRes) {
 		if (!err) {
-		res.redirect('/sessions/new');
+			done(err, dbRes.rows[0]);
 		}
 	});
 });
 
+app.listen(3000, function() {
+	console.log('Server is up!');
+});
+
+var localStrategy = new LocalStrategy(
+  function(username, password, done) {
+    db.query('SELECT * FROM users WHERE username = $1', [username], function(err, dbRes) {
+
+    	console.log(dbRes);
+    	var user = dbRes.rows[0];
+    	console.log(username)
+
+    	console.log(user);
+
+
+      if (err) { return done(err); }
+      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+      return done(null, user);
+    })
+  }
+);
+
+passport.use(localStrategy);
+
+app.get('/', function(req, res) {
+	res.render('index', { user: req.user });
+});
+
+// User routes
+app.get('/users/new', function(req, res) {
+	res.render('users/new');
+});
+
+app.post('/users', function(req, res) {
+	db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [req.body.username, req.body.email, req.body.password], function(err, dbRes) {
+
+		console.log(err)
+			if (!err) {
+				res.redirect('/sessions/new');
+			}
+	});
+});
+
+// Session routes
 app.get('/sessions/new', function(req, res) {
 	res.render('sessions/new');
 });
 
 app.post('/sessions', passport.authenticate('local', 
   {failureRedirect: '/sessions/new'}), function(req, res) {
-    // res.redirect('/');
-    db.query('SELECT * FROM users WHERE email = $1 AND password = $2', [], function() {
-
-    });
+    res.redirect('/');
 });
 
+app.delete('/sessions', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
 
+// Post routes
+app.get('/posts/new', function(req, res) {
+	res.render('posts/new');
+});
 
+app.post('/posts', function(req, res) {
+	db.query('INSERT INTO posts (title, mainbody, user_id) VALUES ($1, $2, $3)', [req.body.title, req.body.mainbody, req.user.id], function (err, dbRes) {
+		if (!err) {
+			res.redirect('/posts');
+		}
+	});
+});
 
+app.get('/posts', function(req, res) {
+	db.query('SELECT * FROM posts', function(err, dbRes) {
+		res.render('posts/index', { posts: dbRes.rows });
+	});
+});
 
+app.get('/posts/:id', function(req, res) {
+	db.query('SELECT * FROM posts WHERE id = $1', [req.params.id], function(err, dbRes) {
+		if (!err) {
+			res.render('posts/show', { post: dbRes.rows[0] });
+		}
+	});
+});
 
+app.get('/posts/:id/edit', function(req, res) {
+	db.query('SELECT * FROM posts WHERE id = $1', [req.params.id], function(err, dbRes) {
+		if (!err) {
+			res.render('posts/edit', { post: dbRes.rows[0] });
+		}
+	});
+});
 
+app.patch('/posts/:id', function(req, res) {
+	db.query('UPDATE posts SET title = $1, mainbody = $2 WHERE id = $3', [req.body.title, req.body.mainbody, req.params.id], function(err, dbRes) {
+		if (!err) {
+			res.redirect('/posts/' + req.params.id);
+		}
+	});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+app.delete('/posts/:id', function(req, res) {
+	db.query('DELETE FROM posts WHERE id = $1', [req.params.id], function(err, dbRes) {
+		if (!err) {
+			res.redirect('/posts');
+		}
+	})
+});
